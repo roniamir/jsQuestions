9a2739a0bc053f32c97f5199d8680cd15e319862
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-18 20:02:05 +0200

    add q1 solution

---
+++ jsQ1.js
@@ -0,0 +1,8 @@
+let number1 = Number(prompt("Please insert your first number"));
+let number2 = Number(prompt("Please insert your second number (choose different number)"));
+if(number1>number2){
+    alert("the larger number is " + number1);
+}
+else{
+    alert("the larger number is " + number2);
+}
