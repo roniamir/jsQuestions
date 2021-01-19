15f36d3bd0b46351fc0661b5841dc7360e8098b7
roniamir <77384841+roniamir@users.noreply.github.com>
2021-01-19 09:15:11 +0200

    q8 edit

--- jsQ8.js
+++ jsQ8.js
@@ -1,24 +1,26 @@
 //Assuming that up to a hundred there are 5 happy numbers
 let countHappy = 0;
-let i = 0;
+let numberQ8 = 13;
+let i = (string(numberQ8)).length;
 let happySumNum = 0;
 
 function digitSquared(num){
-    let sum
-    let sameNum = num;
-    while(sameNum > 0){
-        let digit = sameNum % 10;
+    for (let j = 0; j < i; j++) {
+        happySumNum +=  (Math.floor(i % 10))**2;
+        num = Math.floor(num / 10);
     }
 }
 
 while(countHappy < 5){
-    happySumNum = (Math.floor(i % 10))**2 + (Math.floor(i / 10))**2;
     while(happySumNum > 9){
-        happySumNum = (Math.floor(happySumNum % 10))**2 + (Math.floor(happySumNum / 10))**2;
+        digitSquared(numberQ8);
+    }
+    if(happySumNumb === 4){
+        console.log(i + " is not an happy number");
+
     }
     if(happySumNum == 1){
         console.log(i + " is happy number");
         countHappy++;
     }
-    i++;
 }
